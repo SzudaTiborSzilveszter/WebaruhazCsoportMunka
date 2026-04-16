4f821e847825c6efee25ae1dd89263ed850acd67
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-04-16 12:15:06 +0200

    Kepek es adatlista hozzaadva

---
+++ adat.js
@@ -0,0 +1,38 @@
+export const FEGYVERLISTA = [
+    {
+        nev: "Mjölnir",
+        kep: "Kepek/mjolnir.jpg",
+        ar: "5000 Digit",
+        leiras: "A mennydörgés istene által forgatott pöröly, amely képes hegyeket porrá zúzni és visszatérni forgatója kezébe."
+    },
+    {
+        nev: "Blades of Chaos",
+        kep: "Kepek/bladesofchaos.jpeg",
+        ar: "3000 Digit",
+        leiras: "Az Alvilág tüzében kovácsolt láncos pengék, amelyek minden suhintással izzó pusztítást mérnek az ellenségre."
+    },
+    {
+        nev: "Leviathan Axe",
+        kep: "Kepek/leviathanaxe.png",
+        ar: "4500 Digit",
+        leiras: "Fagyos rúnákkal vésett északi fejsze, amely jéggé dermeszti a leggonoszabb bestiák szívét is."
+    },
+    {
+        nev: "Portal Gun",
+        kep: "Kepek/portalgun.jpeg",
+        ar: "1500 Digit",
+        leiras: "A tér és idő törvényeit meghazudtoló eszköz, amellyel bárki pillanatok alatt áthidalhatja a lehetetlen távolságokat."
+    },
+    {
+        nev: "Crowbar",
+        kep: "Kepek/crowbar.jpeg",
+        ar: "4 Digit",
+        leiras: "Egyszerű acélszerszám, amely egy néma hős kezében a fizika törvényeinél is erősebb fegyverré vált."
+    },
+    {
+        nev: "BFG9000",
+        kep: "Kepek/bfg9000.png",
+        ar: "2500 Digit",
+        leiras: "A technológia csúcsa, amely egyetlen plazmalöketével képes teljes seregeket kitörölni a létezésből."
+    }
+]
