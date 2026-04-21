da0f56ba703bc0db067d6ba0d652795d13ca4007
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-04-21 13:10:44 +0200

    Vege

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const szuloELEM = document.querySelector(".kartyak");
 const kosarELEM = document.querySelector("#kosar");
 const kosarSzamELEM = document.querySelector("#kosarSzam");
 
-<<<<<<< HEAD
 const KOSARBAN = []; 
 kosarELEM.innerHTML = "🛒";
 kosarSzamELEM.innerHTML = "0";
@@ -20,21 +19,4 @@ window.addEventListener("kosarba", function (event) {
 kosarELEM.addEventListener("click", function () {
     const q = encodeURIComponent(KOSARBAN.join(","));
     window.location.href = `kosar.html?items=${q}`;
-=======
-
-
-
-new Termekek(FEGYVERLISTA,szuloELEM);
-const KOSARBAN=[];
-window.addEventListener("kosarba",function(event){
-    console.log(event.detail);
-    KOSARBAN.push(FEGYVERLISTA[event.detail]);
-    new Termekek(KOSARBAN,kosarELEM);
-    console.log(KOSARBAN);
-});
-
-window.addEventListener("ujTermekHozzaadva", function (event) {
-    console.log("Új termék hozzáadva az index-en:", event.detail);
-    new Termekek(FEGYVERLISTA, szuloELEM);
->>>>>>> a4a86e6e06eacb1eecc565f8da09e8f4318795a2
 });
